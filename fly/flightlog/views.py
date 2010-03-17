db53bba4b2787fdb41fbeb1feac8c6584f2bf66a
dkm <dkm@gre.kataplop.net>
2010-03-17 07:40:57 +0100

    addWing/addFlight use django automatic forms. Using generic views to list wings.

--- fly/flightlog/views.py
+++ fly/flightlog/views.py
@@ -1,26 +1,48 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from fly.flightlog.models import Flight, Wing, Location
 from django.shortcuts import render_to_response, get_object_or_404
-from django import forms
+from django.forms import ModelForm
 
-class AddWingForm(forms.Form):
-    name = forms.CharField(max_length=200)
-    purchase_date = forms.DateTimeField()
+class WingForm(ModelForm):
+    class Meta:
+        model = Wing
+
+class FlightForm(ModelForm):
+    class Meta:
+        model = Flight
 
 def index(request):
     latest_flights_list = Flight.objects.all().order_by('-date')[:5]
     return render_to_response('flightlog/index.html', {'latest_flights_list': latest_flights_list})
 
+
+
+def newflight(request):
+    if request.method == 'POST':
+        form = FlightForm(request.POST)
+        if form.is_valid():
+            # w = Wing(name=form.cleaned_data['name'],
+            #          purchase_date=form.cleaned_data['purchase_date'])
+            # w.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = FlightForm() # An unbound form
+
+    return render_to_response('flightlog/newflight.html', {
+            'form': form,
+            })
+
+
 def newwing(request):
     if request.method == 'POST':
-        form = AddWingForm(request.POST)
+        form = WingForm(request.POST)
         if form.is_valid():
             w = Wing(name=form.cleaned_data['name'],
                      purchase_date=form.cleaned_data['purchase_date'])
             w.save()
             return HttpResponseRedirect('/')
     else:
-        form = AddWingForm() # An unbound form
+        form = WingForm() # An unbound form
 
     return render_to_response('flightlog/newwing.html', {
             'form': form,
--- fly/urls.py
+++ fly/urls.py
@@ -1,15 +1,23 @@
 from django.conf.urls.defaults import *
+from fly.flightlog.models import Flight, Wing, Location
+from django.views.generic import list_detail
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+wing_info = {
+    "queryset" : Wing.objects.all(),
+}
+
 urlpatterns = patterns('',
-    # Example:
+    (r'^wing/$', list_detail.object_list, wing_info),
+
     # (r'^fly/', include('fly.foo.urls')),
     (r'^flights/$', 'fly.flightlog.views.index'),
     (r'^flights/(?P<flight_id>\d+)/$', 'fly.flightlog.views.detail'),
     (r'^wing/new/$', 'fly.flightlog.views.newwing'),
+    (r'^flight/new/$', 'fly.flightlog.views.newflight'),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
