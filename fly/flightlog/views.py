dce5111697f810cb1573eb10eb29713842f824ac
marc <marc@etiquette.(none)>
2010-03-26 13:06:46 +0100

    play with query and generic views extensions

--- fly/flightlog/views.py
+++ fly/flightlog/views.py
@@ -3,6 +3,8 @@ from fly.flightlog.models import Flight, Wing, Location
 from django.shortcuts import render_to_response, get_object_or_404
 from django.forms import ModelForm
 
+from django.views.generic import list_detail
+
 class WingForm(ModelForm):
     class Meta:
         model = Wing
@@ -11,6 +13,31 @@ class FlightForm(ModelForm):
     class Meta:
         model = Flight
 
+def wing_detail(request, wing_id):
+    flights = Flight.objects.filter(wing__exact=wing_id)[:5]
+    return list_detail.object_detail(
+        request,
+        queryset = Wing.objects.all(),
+        object_id = wing_id,
+        template_object_name = "wing",
+        template_name = "flightlog/wing_detail.html",
+        extra_context = {"flights" : flights}
+    )
+
+def location_detail(request, location_id):
+    takeoff = Flight.objects.filter(takeoff__exact=location_id)[:5]
+    landing = Flight.objects.filter(landing__exact=location_id)[:5]
+
+    # Use the object_list view for the heavy lifting.
+    return list_detail.object_detail(
+        request,
+        queryset = Location.objects.all(),
+        object_id = location_id,
+        template_object_name = "location",
+        template_name = "flightlog/location_detail.html",
+        extra_context = {"takeoff" : takeoff,
+                         "landing" : landing}
+    )
 
 
 def newflight(request):
@@ -31,10 +58,16 @@ def newflight(request):
 
 
 def flightstats(request, year=None):
+    all = Flight.objects.all()
+
     if year != None:
         queryset = Flight.objects.filter(date__year=year)
     else:
-        queryset = Flight.objects.all()
+        queryset = all
+
+    years = {}
+    for x in all:
+        years[x.date.year] = True
 
     l = len(queryset)
 
@@ -52,5 +85,6 @@ def flightstats(request, year=None):
                                                              'total_distance' : dist,
                                                              'total_flights' : len(queryset),
                                                              'avg_dist_per_flight' : avg_dist_per_flight,
-                                                             'year': year})
+                                                             'year': year,
+                                                             'years': years})
 
