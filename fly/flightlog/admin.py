3784a40a65f213a2e2b7749477da671ae6c0660e
dkm <dkm@gre.kataplop.net>
2010-03-29 21:47:28 +0200

    automagically use a nice map within the admin for geo-aware model

--- fly/flightlog/admin.py
+++ fly/flightlog/admin.py
@@ -1,7 +1,9 @@
 from fly.flightlog.models import Flight,Wing,Location
-from django.contrib import admin
-
+##from django.contrib import admin
+from django.contrib.gis import admin
 
 admin.site.register(Flight)
 admin.site.register(Wing)
-admin.site.register(Location)
+##admin.site.register(Location)
+ 
+admin.site.register(Location, admin.GeoModelAdmin)
